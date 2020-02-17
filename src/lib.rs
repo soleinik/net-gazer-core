df222b66d50fe9d9f2f1caaaa696f24459c1f7a0
soleinik <162591+soleinik@users.noreply.github.com>
2020-02-16 23:46:12 -0600

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,13 @@ pub type CoreMessage = (u8, Vec<u8>);
 pub type CoreSender = Sender<CoreMessage>;
 
 pub trait Plugin<'p, T>: Any + Send + Sync{
-    
+
     fn on_load(&self);
     fn on_unload(&self);
 
     fn get_name(&self) -> &'_ str;
     fn get_id(&self) -> u8;
 
-    fn process(&mut self, tx:&'_ CoreSender, data:&'_ T);
+    fn process(&self, tx:&'_ CoreSender, data:&'_ T);
 }
 
