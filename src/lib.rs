8e6d54c4e0d1e22c3158d0012702e7ca10dea80e
soleinik <162591+soleinik@users.noreply.github.com>
2020-02-17 01:38:29 -0600

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,14 @@ use std::sync::mpsc::Sender;
 pub type CoreMessage = (u8, Vec<u8>);
 pub type CoreSender = Sender<CoreMessage>;
 
-pub trait Plugin<'p, T>: Any + Send + Sync{
+pub trait Plugin<T>: Any + Send + Sync{
 
     fn on_load(&self);
     fn on_unload(&self);
 
-    fn get_name(&self) -> &'_ str;
+    fn get_name(&self) -> &str;
     fn get_id(&self) -> u8;
 
-    fn process(&self, tx:&'_ CoreSender, data:&'_ T);
+    fn process(&self, tx:&CoreSender, data:&T);
 }
 
