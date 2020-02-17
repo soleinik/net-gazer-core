48b2f228f1ac16c46a44934e655165e46a3fb4b0
soleinik <162591+soleinik@users.noreply.github.com>
2020-02-17 14:42:50 -0600

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,14 @@
 use std::any::Any;
 use std::sync::mpsc::Sender;
 use pnet::packet::ethernet::EthernetPacket;
+use pnet::datalink::NetworkInterface;
 
 pub type CoreMessage = (u8, Vec<u8>);
 pub type CoreSender = Sender<CoreMessage>;
 
 pub trait Plugin: Any + Send + Sync{
 
-    fn on_load(&self);
+    fn on_load(&self, iface:&NetworkInterface);
     fn on_unload(&self);
 
     fn get_name(&self) -> &str;
